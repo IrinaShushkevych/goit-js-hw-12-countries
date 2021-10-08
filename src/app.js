1244604838df44c1679f2a20616bf2842db6da87
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-10-08 13:28:18 +0300

    final

---
+++ src/app.js
@@ -0,0 +1,66 @@
+import getCountry from './js/fetchCountries.js';
+import refs from './js/refs.js';
+const debounce = require('debounce');
+
+function hideElement() {
+  refs.countryNone.style.display = 'none';
+  refs.countryInfo.style.display = 'none';
+  refs.countryList.style.display = 'none';
+  removeEvent();
+}
+
+function getCountryFunc(name) {
+  getCountry(name)
+    .then(data => {
+      if (data.length > 1) {
+        setCountryList(data);
+      } else {
+        setCountryInfo(data[0]);
+      }
+    })
+    .catch(error => {
+      hideElement();
+      setCountryNoneMes();
+    });
+}
+
+function setCountryNoneMes() {
+  refs.countryNone.style.display = 'block';
+}
+
+function setCountryList(data) {
+  refs.countryList.style.display = 'block';
+  const list = data.map(el => `<li>${el.name}</li>`);
+  refs.countryList.innerHTML = list.join('');
+  setEventCountry();
+}
+
+function setCountryInfo(data) {
+  refs.countryInfo.style.display = 'block';
+  refs.countryName.textContent = data.name;
+  refs.countryCapital.textContent = data.capital;
+  refs.countryPopulation.textContent = data.population;
+  refs.countryLangList.innerHTML = data.languages.map(el => `<li>${el.name}</li>`).join('');
+  refs.countryFlag.src = data.flag;
+}
+
+function onClickCountryItem(e) {
+  refs.formEl.elements.countryName.value = e.target.textContent;
+  getCountryFunc(e.target.textContent);
+  hideElement();
+}
+
+function setEventCountry() {
+  refs.countryList.addEventListener('click', onClickCountryItem);
+}
+
+function removeEvent() {
+  refs.countryList.removeEventListener('click', onClickCountryItem);
+}
+
+function onInputCountry(e) {
+  hideElement();
+  if (e.target.value) getCountryFunc(e.target.value);
+}
+
+refs.formEl.elements.countryName.addEventListener('input', debounce(onInputCountry, 500));
---
+++ src/js/refs.js
@@ -0,0 +1,13 @@
+const refs = {
+  formEl: document.querySelector('.form-country'),
+  countryNone: document.querySelector('.country-none'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+  countryName: document.querySelector('.country-name'),
+  countryCapital: document.querySelector('.country-capital'),
+  countryPopulation: document.querySelector('.country-population'),
+  countryLangList: document.querySelector('.country-languages-list'),
+  countryFlag: document.querySelector('.country-flag'),
+};
+
+export default refs;
